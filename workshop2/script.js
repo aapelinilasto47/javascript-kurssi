4e304b1dcf5eddf6ddf9b464d8fcda676ae0e9a8
aapeli <aan00001@laurea.fi>
2025-09-10 14:37:50 +0300

    testejä

--- workshop2/script.js
+++ workshop2/script.js
@@ -1,1 +1,19 @@
-document.write("Hello, World!");
+
+let quote = document.getElementById("quote").innerText = "The only way to do great work is to love what you do.";
+let author = document.getElementById("author").innerText = "- Steve Jobs";
+
+
+
+
+let container =  document.querySelectorAll("div.container").forEach(div => {
+    div.style.fontFamily = "Arial, sans-serif";
+    div.style.textAlign = "center";
+    div.style.maxWidth = "600px";
+    div.style.margin = "50px auto";
+    div.style.border = "2px solid black";
+    div.style.padding = "20px";
+    div.style.marginBottom = "20px";
+    div.style.borderRadius = "5px";
+    div.style.backgroundColor = "#f9f9f9";
+    div.style.boxShadow = "0 4px 8px rgba(0, 0, 0, 0.1)";
+});
